b92dca3973107f987a715bb69b03e429e336f408
eshdecin <tanmayesh@gmail.com>
2025-05-14 16:32:56 +0530

    Add homepage to fix deployment

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>TSMilitaryLaw is online and holding the line!</h1>
+      <p>This site will soon become your full-spectrum military law assistant.</p>
+    </div>
+  );
+}
