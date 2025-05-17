a1d0d60252fe2f131d8bc918956e3cd382fdeb5c
eshdecin <tanmayesh@gmail.com>
2025-05-17 12:34:17 +0530

    Update chat.py

--- app/api/chat.py
+++ app/api/chat.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import PyPDFLoader
-from qa_chain import get_chain  # If you have this file, otherwise I'll update
-
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.chat_models import ChatOpenAI
+from qa_chain import get_chain
 import os
 
 router = APIRouter()
@@ -28,7 +26,7 @@ async def chat_endpoint(request: ChatRequest):
     vectorstore = FAISS.load_local(FAISS_INDEX_PATH, embeddings, allow_dangerous_deserialization=True)
 
     docs = vectorstore.similarity_search(query)
-    chain = load_qa_chain(ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), temperature=0), chain_type="stuff")
+    chain = get_chain(ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY")), vectorstore)
     response = chain.run(input_documents=docs, question=query)
 
     return {"response": response}
