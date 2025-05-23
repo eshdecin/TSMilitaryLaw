cbde54eea87a97cd6b36391996c18faaa31eb525
eshdecin <tanmayesh@gmail.com>
2025-05-24 01:19:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from app.api import chat
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://tsmilitarylaw.info"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # CORS Setup
 app.add_middleware(
     CORSMiddleware,
